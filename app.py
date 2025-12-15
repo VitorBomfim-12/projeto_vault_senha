459c18b82c3998eea232d5c1803fb87b6820a741
Hugo leo <hugoleonardo040906@gmail.com>
2025-12-14 22:24:12 -0300

    arrumando import circular e email para usuario

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import os
 from flask import Flask
 from flask_mail import Mail
 from src.extensions import mail
+from dotenv import load_dotenv
+from src.service.email_sender import MailManager
 
-
+load_dotenv()
 app = Flask(__name__,template_folder=os.path.join('src/view','templates'),
             static_folder=os.path.join('src/view','static'))
 
@@ -20,7 +22,7 @@ app.config["SESSION_USE_SIGNER"] = True
 #configuração flask mail
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = os.getenv('DEL_EMAIL')
+app.config['MAIL_USERNAME'] = os.getenv('DEL_MAIL')
 app.config['MAIL_PASSWORD'] = os.getenv('APP_KEY')
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False 
@@ -29,4 +31,6 @@ mail.init_app(app)
 #rotas
 from src.controller.logincontroler import LoginManager
 app.add_url_rule('/','index',LoginManager.index, methods = ["GET","POST"])
+
+
 
--- src/service/email_sender.py
+++ src/service/email_sender.py
@@ -2,7 +2,9 @@ import pymysql
 from src.extensions import mail
 from flask_mail import Message
 from src.service.func_dbManager import DB_MANAGER
-import os, dotenv
+import os
+import dotenv
+from dotenv import load_dotenv
 
 dotenv.load_dotenv()
 
@@ -12,7 +14,7 @@ class MailManager :
     def mail_sender(user_id):
         con = DB_MANAGER.db_connect()
         cur = con.cursor()
-        sql= "SELECT email FROM USUARIOS WHERE id =%s"
+        sql= "SELECT email FROM usuarios WHERE id =%s"
         cur.execute(sql,(user_id,))
         user = cur.fetchone()
         cur.close()
@@ -20,7 +22,7 @@ class MailManager :
         
         
         mfa_code = DB_MANAGER.gerador_mfa(int(user_id))
-        msg =  Message(subject=f" Código de verificação ", sender = os.getenv('DEL_EMAIL'), recipients=[user['email']])
+        msg =  Message(subject=f" Código de verificação ", sender = os.getenv('DEL_MAIL'), recipients=[user['email']])
         msg.body = f''' O seu código de verificação é {mfa_code} '''
         mail.send(msg)
 
