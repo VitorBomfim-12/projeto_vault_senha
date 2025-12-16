ef4ff61521ba41030df00fbf2851960254164758
VitorBomfim-12 <vbomfimcunha@gmail.com>
2025-12-16 20:23:32 -0300

    feat: inserção de request.methos =='GET" nas rotas.

--- app.py
+++ app.py
@@ -9,9 +9,6 @@ app = Flask(__name__,template_folder=os.path.join('src/view','templates'),
             static_folder=os.path.join('src/view','static'))
 
 app.config.from_object(Config)
-
-
-
 mail.init_app(app)
 
 #rotas
@@ -19,3 +16,5 @@ from src.controller.logincontroler import LoginManager
 app.add_url_rule('/','index',LoginManager.index, methods = ["GET","POST"])
 
 
+if __name__ == "__main__":
+    app.run(debug=True)
