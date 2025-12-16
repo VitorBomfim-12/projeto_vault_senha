2feff8e725f4efaa60fe5f6b82f983c10509a20b
VitorBomfim-12 <vbomfimcunha@gmail.com>
2025-12-16 01:46:16 -0300

    feat: configuralção do config.py

--- app.py
+++ app.py
@@ -1,32 +1,17 @@
 import os
 from flask import Flask
-from flask_mail import Mail
 from src.extensions import mail
 from dotenv import load_dotenv
-from src.service.email_sender import MailManager
-
+from config import Config
 load_dotenv()
+
 app = Flask(__name__,template_folder=os.path.join('src/view','templates'),
             static_folder=os.path.join('src/view','static'))
 
+#configuração do app via configuração por objeto
+app.config.from_object(Config)
 
-
-    
-SECRET_KEY = os.getenv('SECRET_KEY')
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"   
-app.config["SESSION_USE_SIGNER"] = True 
-
-
-
-#configuração flask mail
-app.config['MAIL_SERVER']='smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = os.getenv('DEL_MAIL')
-app.config['MAIL_PASSWORD'] = os.getenv('APP_KEY')
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False 
-
+#inicialização da extensão do flask mail
 mail.init_app(app)
 #rotas
 from src.controller.logincontroler import LoginManager
--- config.py
+++ config.py
@@ -1,5 +1,23 @@
 import os,dotenv
 from dotenv import load_dotenv
-from flask_mail import Mail
-
+from datetime import timedelta
 class Config:
+    load_dotenv()            
+    #definição da secret key do app
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    #configuração dos parâmetros das sessions
+    SESSION_PERMANENT = False
+    SESSION_TYPE = "filesystem"   
+    SESSION_USE_SIGNER = True
+    PERMANENT_SESSION_LIFETIME = timedelta(minutes=30)
+    SESSION_COOKIES_SECURE = True
+    SESSION_COOKIE_HTTPONLY = True
+
+    #configuração flask mail
+    MAIL_SERVER='smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USERNAME = os.getenv('DEL_MAIL')
+    MAIL_PASSWORD = os.getenv('APP_KEY')
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False 
+        
