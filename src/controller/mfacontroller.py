35079677df5298acbf8d1e00e5cb3bbbeaa37995
VitorBomfim-12 <vbomfimcunha@gmail.com>
2025-12-17 18:32:18 -0300

    feat:criação e inserção de decorator para proteção de rotas sensíveis

--- src/controller/mfacontroller.py
+++ src/controller/mfacontroller.py
@@ -1,12 +1,14 @@
 from flask import session,request,redirect,url_for,render_template
 from src.service.email_sender import MailManager
 from src.service.func_dbManager import DB_MANAGER
+from src.service.log_req import login_required
 
 
 
 class MfaVerify:
 
     @staticmethod
+    @login_required
     def mfa():
 
         user_log_attempt = session.get("user_login_attempt",None)
@@ -18,7 +20,7 @@ class MfaVerify:
         if request.method =='POST':
 
             cod_mfa = request.form.get("MFA")
-            attempt = DB_MANAGER.indetificar_mfa(user_log_attempt,cod_mfa)
+            attempt = DB_MANAGER.indetifica_mfa(user_log_attempt,cod_mfa)
 
             if not user_log_attempt or not user_type or not attempt: 
                 return (redirect(url_for('index',error = 'Algo deu errado!')))
