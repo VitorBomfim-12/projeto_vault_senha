28da35ce3a0121f1846305d12fa4102c8ff21801
VitorBomfim-12 <vbomfimcunha@gmail.com>
2025-12-21 23:53:16 -0300

    fix: correção de erro de lógica na geração de senhas, verificação da existência de todos os caracteres

--- src/service/keygen.py
+++ src/service/keygen.py
@@ -1,26 +1,46 @@
-import random
+import random,re
 
 def password_gen(maiusculas:bool, simbolos :bool, numeros : bool, tamanho:int) -> str:
    
+    qualidade_senha=True
     tamanho = int(tamanho)
     maius = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     minus = maius.lower()
     num = '123456789'
     sim ='!@#$%¨&*()'
+
+    
     caracteres_permitidos=""
     caracteres_escolhidos=""
     caracteres_permitidos+=minus
-    
+    padrao= f"["
+    padrao+= re.escape(minus)
     if maiusculas:
         caracteres_permitidos +=maius
+        padrao+= re.escape(maius)
     if simbolos:
         caracteres_permitidos+=sim
+        padrao+= re.escape(sim)
     if numeros:
         caracteres_permitidos+=num
+        padrao+= re.escape(num)
 
-    for i in range(0,tamanho):
-        caracter_sorteado = random.randint(0,len(caracteres_permitidos)-1)
-        caracteres_escolhidos += caracteres_permitidos[caracter_sorteado]
+    padrao+="]"
+    while True:    
+        caracteres_escolhidos=""
 
-    return caracteres_escolhidos
+        for i in range(0,tamanho-1):
+            caracter_sorteado = random.randrange(0,len(caracteres_permitidos))
+            caracteres_escolhidos += caracteres_permitidos[caracter_sorteado]
+        if re.fullmatch(f'{padrao}+',caracteres_escolhidos):
+           return caracteres_escolhidos
+         
+
+
+
+                
+                
+                
+            
+    
 
