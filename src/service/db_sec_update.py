947f99279c66ee31f65dfb0127a38cba67256a45
VitorBomfim-12 <vbomfimcunha@gmail.com>
2025-12-30 05:19:26 -0300

    feat: inserção de feedback de senha segura para o usuário

--- src/service/db_sec_update.py
+++ src/service/db_sec_update.py
@@ -1,5 +1,6 @@
 import pymysql,os,dotenv
 from src.service.password_verify import verifica_senha_api as verify_pass
+from src.service.func_dbManager import DB_MANAGER as db
 dotenv.load_dotenv()
 
 class Db_Update:
@@ -20,7 +21,7 @@ class Db_Update:
         con = Db_Update.db_connect()
         cur = con.cursor()
         sql = "SELECT id_senha,senha_hash FROM senha WHERE user_id_FK = %s"
-        cur.execute(sql,user_id)
+        cur.execute(sql,(user_id,))
         senhas_usuario = cur.fetchall()
         for senha in senhas_usuario:
             status_senha = verify_pass(senha['senha_hash'],False)
@@ -28,7 +29,14 @@ class Db_Update:
             if status_senha:
                 alteracao = True
                 sql='UPDATE senha SET senha_segura =%s WHERE id_senha = %s'
+                print (db.descrip_senha(senha['senha_hash']))
                 cur.execute(sql,(0,senha['id_senha']))
+            else:
+                alteracao = True
+                sql='UPDATE senha SET senha_segura =%s WHERE id_senha = %s'
+                print (db.descrip_senha(senha['senha_hash']))
+                cur.execute(sql,(1,senha['id_senha']))
+               
 
         if alteracao:     
          con.commit()
