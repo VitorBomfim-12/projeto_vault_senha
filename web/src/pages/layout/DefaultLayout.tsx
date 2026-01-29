32dc5ae16c49281a82d239064ae8ede27eda1409
Tais Souza <tata.ssouz47@gmail.com>
2026-01-28 21:04:34 -0300

    style: ajustes na sidebar e correção da exibição da parte de conteúdo

--- web/src/pages/layout/DefaultLayout.tsx
+++ web/src/pages/layout/DefaultLayout.tsx
@@ -1,14 +1,18 @@
 import { Outlet } from "react-router-dom"
 import Sidebar from "../../components/Sidebar"
 import type { Dispatch, SetStateAction } from "react"
+import Header from "../../components/Header"
 
 function DefaultLayout({isOpen, setIsOpen}: {isOpen: boolean,setIsOpen:Dispatch<SetStateAction<boolean>>}) {
     return (
         <main className="main">
             <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
-            <section className="content">
-                
-                <Outlet />
+            <section className="page">
+                <Header />
+                <div className="content">
+                    <Outlet />
+                </div>
+                {!isOpen && <footer className="footer"><p>©  Vault 76 | Todos os direitos reservados </p></footer>}
             </section>
         </main>
     )
